6548540711b1681018ba842a67f5344b64318d74
gemma gonputh <gemmage3@hotmail.co.uk>
2020-02-28 16:27:54 -0300

    Signout button

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'hairs#index'
 
-  resources :hairs do
-    collection do
-      get 'my_hair'
+  # devise_scope :user do
+    # get '/users/sign_out' => 'devise/#destroy'
+
+    resources :hairs do
+      collection do
+        get 'my_hair'
+      end
     end
-  end
+  # end
 
   resources :purchases, only: [ :show, :index, :new, :create ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
