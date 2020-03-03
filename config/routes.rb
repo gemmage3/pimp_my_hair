28d71dcb498d762a67fc8376da737133c35a2d49
Diego Unternaehrer <diego.unternaehrer@hotmail.com>
2020-03-03 15:21:31 -0300

    Added carousel and icon to the cards

--- config/routes.rb
+++ config/routes.rb
@@ -16,11 +16,5 @@ Rails.application.routes.draw do
   end
 
   resources :purchases, only: [ :show ]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'infos/why_us', to: 'infos#why', as: :info_why
-  get 'infos/experience', to: 'infos#experience', as: :info_experience
-  get 'infos/faq', to: 'infos#faq', as: :info_faq
-  get 'infos/affiliate_link', to: 'infos#affiliate', as: :info_affiliate
-  get 'infos/shipping_prices', to: 'infos#shipping', as: :info_shipping
-  get 'infos/help', to: 'infos#help', as: :info_help
+
 end
